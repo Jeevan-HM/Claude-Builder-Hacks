4e85ea74b0b6c171875b95b9692136aa410a5472
Varun <varun@Varuns-MacBook-Pro.local>
2025-11-08 21:12:54 -0700

    added team dashboard route and template rendering

--- app.py
+++ app.py
@@ -8,5 +8,10 @@ def index():
     return render_template("index.html")
 
 
+@app.route("/team-dashboard")
+def team_dashboard():
+    return render_template("team_dashboard.html")
+
+
 if __name__ == "__main__":
     app.run(debug=True)
